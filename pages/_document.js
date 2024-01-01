c7de41d8dd712fc7dc57d4917eff21e286e920e2
Bartas Urba <77767904+barturba@users.noreply.github.com>
2023-12-31 21:22:43 -0700

    Updated meta data.
    undefined

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Bartas Urba',
+    description:
+      'Innovative and driven IT professional with a proven track record in system optimization, seeking to apply my expertise and collaborative spirit to advance technological operations within a dynamic organization.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
